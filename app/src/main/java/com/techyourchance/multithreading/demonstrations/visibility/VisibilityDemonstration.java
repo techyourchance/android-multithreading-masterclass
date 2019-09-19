cfff0f1aabaab84822025de2f5b5b2899b52c39b
Vasiliy Zukanov <techyourchance@gmail.com>
2019-09-19 14:03:35 +0300

    Visibility issue demonstration

---
+++ app/src/main/java/com/techyourchance/multithreading/demonstrations/visibility/VisibilityDemonstration.java
@@ -0,0 +1,52 @@
+package com.techyourchance.multithreading.demonstrations.visibility;
+
+public class VisibilityDemonstration {
+
+    private static int sCount = 0;
+
+    public static void main(String[] args) {
+        new Consumer().start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            return;
+        }
+        new Producer().start();
+    }
+
+    static class Consumer extends Thread {
+        @Override
+        public void run() {
+            int localValue = -1;
+            while (true) {
+                if (localValue != sCount) {
+                    System.out.println("Consumer: detected count change " + sCount);
+                    localValue = sCount;
+                }
+                if (sCount >= 5) {
+                    break;
+                }
+            }
+            System.out.println("Consumer: terminating");
+        }
+    }
+
+    static class Producer extends Thread {
+        @Override
+        public void run() {
+            while (sCount < 5) {
+                int localValue = sCount;
+                localValue++;
+                System.out.println("Producer: incrementing count to " + localValue);
+                sCount = localValue;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+            System.out.println("Producer: terminating");
+        }
+    }
+
+}
