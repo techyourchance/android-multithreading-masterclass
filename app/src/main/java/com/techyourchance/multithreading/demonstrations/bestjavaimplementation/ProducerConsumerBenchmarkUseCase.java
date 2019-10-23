97358d38580a94aaf165a7a41a528a137d5e2b0f
Vasiliy Zukanov <techyourchance@gmail.com>
2019-10-23 19:34:42 +0300

    Best implementations of Benchmark and Factorial in Java

---
+++ app/src/main/java/com/techyourchance/multithreading/demonstrations/bestjavaimplementation/ProducerConsumerBenchmarkUseCase.java
@@ -0,0 +1,120 @@
+package com.techyourchance.multithreading.demonstrations.bestjavaimplementation;
+
+import com.techyourchance.multithreading.common.BaseObservable;
+import com.techyourchance.threadposter.BackgroundThreadPoster;
+import com.techyourchance.threadposter.UiThreadPoster;
+
+public class ProducerConsumerBenchmarkUseCase extends BaseObservable<ProducerConsumerBenchmarkUseCase.Listener> {
+
+    public static interface Listener {
+        void onBenchmarkCompleted(Result result);
+    }
+
+    public static class Result {
+        private final long mExecutionTime;
+        private final int mNumOfReceivedMessages;
+
+        public Result(long executionTime, int numOfReceivedMessages) {
+            mExecutionTime = executionTime;
+            mNumOfReceivedMessages = numOfReceivedMessages;
+        }
+
+        public long getExecutionTime() {
+            return mExecutionTime;
+        }
+
+        public int getNumOfReceivedMessages() {
+            return mNumOfReceivedMessages;
+        }
+    }
+
+    private static final int NUM_OF_MESSAGES = 1000;
+    private static final int BLOCKING_QUEUE_CAPACITY = 5;
+
+    private final Object LOCK = new Object();
+
+    private final UiThreadPoster mUiThreadPoster = new UiThreadPoster();
+    private final BackgroundThreadPoster mBackgroundThreadPoster = new BackgroundThreadPoster();
+
+    private final MyBlockingQueue mBlockingQueue = new MyBlockingQueue(BLOCKING_QUEUE_CAPACITY);
+
+    private int mNumOfFinishedConsumers;
+
+    private int mNumOfReceivedMessages;
+
+    public void startBenchmarkAndNotify() {
+        mBackgroundThreadPoster.post(() -> {
+
+            mNumOfReceivedMessages = 0;
+            mNumOfFinishedConsumers = 0;
+            long startTimestamp = System.currentTimeMillis();
+
+            // producers init thread
+            mBackgroundThreadPoster.post(() -> {
+                for (int i = 0; i < NUM_OF_MESSAGES; i++) {
+                    startNewProducer(i);
+                }
+            });
+
+            // consumers init thread
+            mBackgroundThreadPoster.post(() -> {
+                for (int i = 0; i < NUM_OF_MESSAGES; i++) {
+                    startNewConsumer();
+                }
+            });
+
+            waitForAllConsumersToFinish();
+
+            Result result;
+            synchronized (LOCK) {
+                result = new Result(
+                        System.currentTimeMillis() - startTimestamp,
+                        mNumOfReceivedMessages
+                );
+            }
+
+            notifySuccess(result);
+            
+        });
+
+    }
+
+    private void waitForAllConsumersToFinish() {
+        synchronized (LOCK) {
+            while (mNumOfFinishedConsumers < NUM_OF_MESSAGES) {
+                try {
+                    LOCK.wait();
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        }
+    }
+
+    private void startNewProducer(final int index) {
+        mBackgroundThreadPoster.post(() -> mBlockingQueue.put(index));
+    }
+
+    private void startNewConsumer() {
+        mBackgroundThreadPoster.post(() -> {
+            int message = mBlockingQueue.take();
+            synchronized (LOCK) {
+                if (message != -1) {
+                    mNumOfReceivedMessages++;
+                }
+                mNumOfFinishedConsumers++;
+                LOCK.notifyAll();
+            }
+        });
+    }
+
+    private void notifySuccess(Result result) {
+        mUiThreadPoster.post(() -> {
+            for (Listener listener : getListeners()) {
+                listener.onBenchmarkCompleted(result);
+            }
+        });
+    }
+
+
+}
