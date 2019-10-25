c37cf161509d752ea468d688ca462f43b73a1fa9
Vasiliy Zukanov <techyourchance@gmail.com>
2019-10-25 11:58:12 +0300

    Adding configurable delay to all producers in the app

--- app/src/main/java/com/techyourchance/multithreading/DefaultConfiguration.java
+++ app/src/main/java/com/techyourchance/multithreading/DefaultConfiguration.java
@@ -4,6 +4,7 @@ public class DefaultConfiguration {
 
     public static final int DEFAULT_NUM_OF_MESSAGES = 1000;
     public static final int DEFAULT_BLOCKING_QUEUE_SIZE = 5;
+    public static final int DEFAULT_PRODUCER_DELAY_MS = 0;
 
     public static final int DEFAULT_FACTORIAL_TIMEOUT_MS = 1000;
 
